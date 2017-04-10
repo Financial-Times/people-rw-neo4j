807808ee10caa86a8e01cc8e24bc848088428b64
Tamas Molnar <tamas.molnar@ft.com>
2017-04-10 17:39:53 +0300

    Added logging and extra CR stages, slack integration

--- jenkins-scripts/deployRelease.groovy
+++ jenkins-scripts/deployRelease.groovy
@@ -1,65 +1,96 @@
-CREDENTIALS_DIR="credentials"
-DOCKER_IMAGE_ID="coco/k8s-cli-utils:latest"
-APP_NAME='people-rw-neo4j'
+CREDENTIALS_DIR = "credentials"
+SLACK_HOOK = "foobar"
+DOCKER_IMAGE_ID = "coco/k8s-cli-utils:latest"
+APP_NAME = 'people-rw-neo4j'
+PRE_PROD_ENV = "foo-pre-prod"
+PROD_ENV = "foo-prod"
 
 node {
-  catchError {
-    stage 'checkout'
+    catchError {
+        stage 'checkout'
 
-    echo "Checking out tag $GIT_TAG"
-    checkout([$class: 'GitSCM', branches: [[name: "refs/tags/$GIT_TAG"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Financial-Times/people-rw-neo4j']]])
+        echo "Checking out tag $GIT_TAG"
+        checkout([$class: 'GitSCM', branches: [[name: "refs/tags/$GIT_TAG"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Financial-Times/people-rw-neo4j']]])
 
-    stage "prepare credentials"
-    prepareCredentials()
+        stage "prepare credentials"
+        prepareCredentials()
 
-    stage 'build-image'
-    DOCKER_TAG = "coco/${APP_NAME}:${GIT_TAG}"
-    echo "Building image $DOCKER_TAG"
-    // docker.build("coco/${APP_NAME}:pipeline${GIT_TAG}", ".")
+        stage 'build-image'
+        DOCKER_TAG = "coco/${APP_NAME}:${GIT_TAG}"
+        echo "Building image $DOCKER_TAG"
+        docker.build("coco/${APP_NAME}:pipeline${GIT_TAG}", ".")
 
-    stage 'push-image'
-    echo "TODO Push the image to dockerhub"
+        stage 'push-image'
+        echo "Pushing image ${DOCKER_TAG} to dockerhub"
+        echo "TODO Push the image to dockerhub"
 
-    stage 'CR for PRE-PROD'
-    echo "TODO CR for PRE-PROD"
+        stage 'Open CR for PRE-PROD'
+        echo "Opening CR for deployment to PRE-PROD."
+        echo "TODO open CR for PRE-PROD"
 
-    stage 'deploy-to-pre-prod'
-    String currentDir = pwd()
-    docker.image(DOCKER_IMAGE_ID).inside("-v ${currentDir}/${CREDENTIALS_DIR}:/${CREDENTIALS_DIR}") {
-      sh "kubectl get pods --selector=app=topics-rw-neo4j -o jsonpath='{\$.items[0].spec.containers[*].image}'"
-      //sh "kubectl set image deployments/${APP_NAME} ${APP_NAME}=\"coco/${APP_NAME}:v${GIT_TAG}\""
+        stage 'deploy-to-pre-prod'
+        String currentDir = pwd()
+        docker.image(DOCKER_IMAGE_ID).inside("-v ${currentDir}/${CREDENTIALS_DIR}:/${CREDENTIALS_DIR}") {
+            sh "kubectl get pods --selector=app=topics-rw-neo4j -o jsonpath='{\$.items[0].spec.containers[*].image}' > image-version"
+            echo "pre-prod old version: " + readFile("image-version")
+
+            sh "kubectl set image deployments/${APP_NAME} ${APP_NAME}=\"coco/${APP_NAME}:v${GIT_TAG}\""
+
+            sh "kubectl get pods --selector=app=topics-rw-neo4j -o jsonpath='{\$.items[0].spec.containers[*].image}' > image-version"
+            echo "pre-prod new version: " + readFile("image-version")
+        }
+
+        stage 'Close CR for PRE-PROD'
+        echo "Closing CR for deployment to PRE-PROD."
+        echo "TODO close CR for PRE-PROD"
+
+        stage 'Validate in PRE-PROD'
+        echo "Starting manual validation in PRE-PROD"
+        sh "curl -X POST --data-urlencode 'payload={\"username\": \"Jenkins\", \"text\": \"Manual action needed: <${env.BUILD_URL}input|click here to validate deployment to PRE-PROD>\", \"icon_emoji\": \":k8s:\"}' ${SLACK_HOOK}"
+        input message: "Check the app in pre-prod: https://$PRE_PROD_ENV/__health/__pods-health?service-name=${APP_NAME}", ok: 'App is OK in PRE-PROD'
+
+        stage 'Deploy to PROD'
+        echo "TODO slack or email integration fro deployment to PROD"
+        input message: 'Press the button to deploy to prod', ok: 'Deploy to PROD'
+
+        stage 'Open CR for PROD'
+        echo "Opening CR for deployment to PRE-PROD."
+        echo "TODO CR for PROD"
+
+        stage 'deploy-to-prod'
+        docker.image(DOCKER_IMAGE_ID).inside("-v ${currentDir}/${CREDENTIALS_DIR}:/${CREDENTIALS_DIR}") {
+            sh "kubectl get pods --selector=app=${APP_NAME} -o jsonpath='{\$.items[0].spec.containers[*].image}' > image-version"
+            echo "prod old version: " + readFile("image-version")
+
+            sh "kubectl set image deployments/${APP_NAME} ${APP_NAME}=\"coco/${APP_NAME}:v${GIT_TAG}\""
+
+            sh "kubectl get pods --selector=app=${APP_NAME} -o jsonpath='{\$.items[0].spec.containers[*].image}' > image-version"
+            echo "prod new version: " + readFile("image-version")
+        }
+
+        stage 'Close CR for PROD'
+        echo "Closing CR for deployment to PRE-PROD."
+        echo "TODO CR for PROD"
+
+        stage 'Validate in PROD'
+        echo "Starting manual validation in PROD"
+        echo "TODO slack or email integration for deployment to PROD"
+        input message: 'Check the app in PROD https://${PROD_ENV}/__health/__pods-health?service-name=${APP_NAME}', ok: 'App is OK in PROD'
     }
 
-    stage 'Validate in PRE-PROD'
-    input message: 'Check the app in pre-prod', ok: 'App is ok in PRE-PROD'
-
-    stage 'Deploy to PROD'
-    input message: 'Press the button to deploy to prod', ok: 'Deploy to PROD'
-
-    stage 'CR for PROD'
-    echo "TODO CR for PROD"
-
-    stage 'deploy-to-prod'
-    docker.image(DOCKER_IMAGE_ID).inside("-v ${currentDir}/${CREDENTIALS_DIR}:/${CREDENTIALS_DIR}") {
-      //sh "kubectl set image deployments/${APP_NAME} ${APP_NAME}=\"coco/${APP_NAME}:v${GIT_TAG}\""
-    }
-
-    stage 'Validate in PROD'
-    input message: 'Check the app in PROD', ok: 'App is OK in PROD'
-  }
-  deleteDir() 
+    deleteDir()
 }
 
 public prepareCredentials() {
-  withCredentials([
-      [$class: 'FileBinding', credentialsId: 'ft.k8s.client-certificate', variable: 'CLIENT_CERT'],
-      [$class: 'FileBinding', credentialsId: 'ft.k8s.ca-cert', variable: 'CA_CERT'],
-      [$class: 'FileBinding', credentialsId: 'ft.k8s.client-key', variable: 'CLIENT_KEY']]) {
-    sh """
+    withCredentials([
+            [$class: 'FileBinding', credentialsId: 'ft.k8s.client-certificate', variable: 'CLIENT_CERT'],
+            [$class: 'FileBinding', credentialsId: 'ft.k8s.ca-cert', variable: 'CA_CERT'],
+            [$class: 'FileBinding', credentialsId: 'ft.k8s.client-key', variable: 'CLIENT_KEY']]) {
+        sh """
       mkdir -p ${CREDENTIALS_DIR}
       cp ${env.CLIENT_CERT} ${CREDENTIALS_DIR}/
       cp ${env.CLIENT_KEY} ${CREDENTIALS_DIR}/
       cp ${env.CA_CERT} ${CREDENTIALS_DIR}/
     """
-  }
+    }
 }
